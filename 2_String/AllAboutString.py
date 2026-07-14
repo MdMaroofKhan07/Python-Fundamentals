87a391623cbd60bad15df9305b72b38d57fbc6c6
Md Maroof Khan <kmaroof1107@gmail.com>
2026-07-14 13:12:55 +0530

    Add files via upload

---
+++ 2_String/AllAboutString.py
@@ -0,0 +1,14 @@
+str1 = "MdMaroof"
+print(str1)
+print(str1[4])
+print("The length of str1 is",len(str1))
+str2 = "Khan"
+print(str2)
+print("The length of str2 is",len(str2))
+final_str = str1 + str2
+print(final_str)
+print("The length of final_str is",len(final_str))
+print(final_str[1])
+print(final_str[3]) #Because indexing starts with 0
+print(final_str[0:4]) #SLICING Output MdMa index : 0-3
+
