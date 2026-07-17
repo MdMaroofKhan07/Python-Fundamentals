be669e7f098b0993200a61fc8af07aae8acac294
Md Maroof Khan <kmaroof1107@gmail.com>
2026-07-17 22:32:40 +0530

    Delete List1.py

--- List1.py
+++
@@ -1,9 +0,0 @@
-marks = [ "Maroof",576.4,76.8,"Zaid",23.4 ]
-print(marks)
-print(type(marks))
-print(marks[0])
-print(marks[2])
-print(len(marks))
-marks[0] = "Hello"
-print(marks)
-print(marks[1:4])#SLICING
