dcf507ae6bcbade0ce78e270c79eb2af9746e550
Md Maroof Khan <kmaroof1107@gmail.com>
2026-07-18 22:42:11 +0530

    Add files via upload

---
+++ DictionaryNested.py
@@ -0,0 +1,10 @@
+students = {
+    "name" : "Md Maroof Khan",
+    "subjects" : {
+        "phy" : 89,
+        "chem" : 68,
+        "math" : 87
+    }
+}
+print(students)
+print(students["subjects"]["chem"])
