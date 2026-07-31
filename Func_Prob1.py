e726bb00a8f68621f1704aae99447c9245666a6c
Md Maroof Khan <kmaroof1107@gmail.com>
2026-07-31 18:45:04 +0530

    Add files via upload

---
+++ Func_Prob1.py
@@ -0,0 +1,10 @@
+# WAF to print the length of a list. ( List is the parameter )
+
+cities = [ "Delhi","Mumbai","Ghazipur","Aligarh" ]
+heroes = [ "Thor","Iron Man","Superhero" ]
+
+def print_len(list):
+    print(len(list))
+
+print_len(cities)
+print_len(heroes)
