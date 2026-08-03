ff210a772bb9a9d8b1db39f0fd699ba55266bdd1
Md Maroof Khan <kmaroof1107@gmail.com>
2026-08-03 22:05:02 +0530

    Add files via upload

---
+++ 7_Function_Recursion/Function1.py
@@ -0,0 +1,18 @@
+# def calc_sum( a,b ):
+#     sum = a + b
+#     print(sum)
+#     return sum
+
+# calc_sum( 4,5 )
+
+# calc_sum( 9,5 )
+
+# calc_sum( 3,2 )
+
+
+# Function definition
+def calc_sum( a,b ): #parameters
+    return a + b
+
+sum = calc_sum( 3,5 ) # function call , Arguments
+print(sum)
