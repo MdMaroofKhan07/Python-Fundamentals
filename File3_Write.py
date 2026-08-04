cc6a84c093077829e35128624190c78de13708c5
Md Maroof Khan <kmaroof1107@gmail.com>
2026-08-04 18:20:46 +0530

    Add files via upload

---
+++ File3_Write.py
@@ -0,0 +1,6 @@
+f = open("demo.txt","w")
+
+f.write("I want to learn JavaScript From tommorow\n")
+f.write("I want to learn Python From now")
+
+f.close()
---
+++ File4_Apend.py
@@ -0,0 +1,5 @@
+f = open("Demo.txt","a")
+
+f.write("\nThen I will move to React.js")
+
+f.close()
